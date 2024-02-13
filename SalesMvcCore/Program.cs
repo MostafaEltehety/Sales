c7378bd9398a4ca186f34fbfc8a048fb9b05f0f6
MostafaEltehety <mostafa.eltehety@gmail.com>
2024-02-14 00:15:43 +0200

    Layout

--- SalesMvcCore/Program.cs
+++ SalesMvcCore/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using SalesMvcCore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<SalesDbContext>(opition => opition.UseSqlServer(builder.Configuration.GetConnectionString("con")));
 
 var app = builder.Build();
 
